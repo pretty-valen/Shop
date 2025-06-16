7119a77ac92d219e2cf30130ccff48cc3d26811d
talok100uraba <talok100uraba@gmail.com>
2025-06-16 16:09:48 -0500

    Conectar login con backend en Render

--- admin.js
+++ admin.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const u = document.getElementById("admin-user").value.trim();
     const p = document.getElementById("admin-pass").value.trim();
 
-    const res = await fetch("http://localhost:3000/login", {
+    const res = await fetch("https://admin-backend-ts85.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ usuario: u, clave: p })
