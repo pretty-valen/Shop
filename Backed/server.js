9438cf90a846cfaaac90054bd94d30ff6fb8072f
talok100uraba <talok100uraba@gmail.com>
2025-06-17 13:10:55 -0500

    Actualizar URI con &tls=true en .env

--- Backed/server.js
+++ Backed/server.js
@@ -11,8 +11,11 @@ const PORT = process.env.PORT || 3000;
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  ssl: true,
+  tlsAllowInvalidCertificates: false
 });
+
 
 // Middleware con límite de carga aumentado
 app.use(cors());
